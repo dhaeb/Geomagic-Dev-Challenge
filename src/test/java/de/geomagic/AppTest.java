66e55fb7c361a78d3f7f558aba3ffe31c3b33802
Dan Häberlein <dh@lodgit.com>
2022-02-16 21:04:17 +0100

    init java 17 project

---
+++ src/test/java/de/geomagic/AppTest.java
@@ -0,0 +1,14 @@
+package de.geomagic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+
+}
