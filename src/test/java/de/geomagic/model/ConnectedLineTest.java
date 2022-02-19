13a9dc417430f6f97ce282334758146546e74ef8
Dan Häberlein <dh@lodgit.com>
2022-02-19 09:10:45 +0100

    getEndpoints for ConnectedLine

--- src/test/java/de/geomagic/model/ConnectedLineTest.java
+++ src/test/java/de/geomagic/model/ConnectedLineTest.java
@@ -1,11 +1,15 @@
 package de.geomagic.model;
 
+import de.geomagic.Line;
+import de.geomagic.types.OrderAgnosticPair;
+import lombok.Generated;
 import lombok.val;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.paukov.combinatorics3.Generator;
 
 import java.util.List;
 import java.util.Set;
@@ -20,12 +24,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class ConnectedLineTest {
 
+    @ParameterizedTest(name = "{2}")
+    @MethodSource("getArgsForTestGetEndpoints")
+    void testGetEndpoints(List<SimpleLine> input, OrderAgnosticPair<Point2D, Point2D> expected, String desc) {
+        val testable = new ConnectedLine(input);
+        assertEquals(expected, testable.getEndpoints(), "Failed: " + desc );
+    }
+
+    /**
+     * Arguments for testing testGetEndpoints
+     *
+     * @return each Stream entry will result into a test in {@code testGetEndpoints}
+     */
+    private static Stream<Arguments> getArgsForTestGetEndpoints() {
+        val fixtures = new TestConstants();
+        val pair_0_0_and_3_3 = OrderAgnosticPair.of(fixtures.getOrigin(), fixtures.getPoint_3_3());
+        val pair_3_3_and_0_0 = OrderAgnosticPair.of(fixtures.getPoint_3_3(), fixtures.getOrigin());
+        return Stream.concat(
+                Generator.permutation(fixtures.getLine_1_1__2_2(), fixtures.getLine_3_3__2_2(), fixtures.getLine_0_0__1_1())
+                        .simple()
+                        .stream()
+                        .map(perm -> Arguments.of(perm, pair_0_0_and_3_3, "line with 3 params should have two endpoints: perm " + perm)),
+                Generator.permutation(fixtures.getLine_1_1__2_2(), fixtures.getLine_3_3__2_2(), fixtures.getLine_0_0__1_1())
+                        .simple()
+                        .stream()
+                        .map(perm -> Arguments.of(perm, pair_3_3_and_0_0, "line with 3 params should have two endpoints: perm " + perm))
+        );
+
+    }
+
     @ParameterizedTest
     @MethodSource("getArgsForTestSumAllEuclideanDistances")
     void testSumAllEuclideanDistances(List<SimpleLine> input , double expected) {
         val testable = new ConnectedLine(input);
         assertEquals(expected, testable.sumAllEuclideanDistances(), new TestConstants().getTestDelta());
-
     }
 
     /**
@@ -38,17 +70,17 @@ class ConnectedLineTest {
         return Stream.of(
                 Arguments.of(List.of(fixtures.getLine_0_1__1_1(),
                                      fixtures.getLine_1_1__1_2()),
-                        2
+                       /*Expected length: */ 2
                 ),
                 Arguments.of(List.of(fixtures.getLine_0_0__1_1(),
                                 fixtures.getLine_1_1__2_2(),
                                fixtures.getLine_2_2__2_3()),
-                        1 + 1.414 * 2 //(1 + √2 * 2)
+                        /*Expected length: */ 1 + 1.414 * 2 //(1 + √2 * 2)
                 ),
                 Arguments.of(List.of(fixtures.getLine_0_0__1_1(),
                         fixtures.getLine_1_1__2_2(),
                         fixtures.getLine_2_2__3_3()),
-                        3 * 1.414 //(3 * √2)
+                        /*Expected length: */ 3 * 1.414 //(3 * √2)
                 )
         );
     }
